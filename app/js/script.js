0674be67233bc31d4bd89b2ac0b45c67c9dfcdd4
Rajeev Nomad <33623043+RajeevNomad@users.noreply.github.com>
2021-03-29 05:18:07 +0530

    Mobile/Tablet Site Optimization

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,14 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const mobilemenu = document.querySelector('#mobilemenu');
 
 btnHamburger.addEventListener('click', function() {
     console.log('hello');
     if(btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open')
+        btnHamburger.classList.remove('open');
+        mobilemenu.classList.remove('show');
     }
     else {
-        btnHamburger.classList.add('open')
+        btnHamburger.classList.add('open');
+        mobilemenu.classList.add('show');
     }
 });
